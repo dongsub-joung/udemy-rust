b2df6577dfb8165e28dd87ad2e1a0ec36f756a9d
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-07-30 17:03:42 +0900

    section1 - 2

---
+++ David-Choi/Twitter-Api-Clone/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::HttpServer;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(||{
+        actix_web::App::new()
+            .service(
+                actix_web::web::scope("/v1")
+                    .route("/profile", actix_web::web::get().to(index))
+                    .route("/profile", actix_web::web::post().to(insert))
+            )
+    })
+    .bind(("127.0.0.1", 8001))?
+    .run()
+    .await
+}
+
+async fn index() -> &'static str{
+    "hellow"
+}
+
+async fn insert() -> &'static str{
+    "inserted"
+}
